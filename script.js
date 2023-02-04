7be7e5ec39e37be86bd710191de500526c8338cd
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-02-04 16:14:28 -0300

    Lógica da programação orientada a objeto implement

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+class Form{
+  items = []
+  method = 'GET'
+  constructor(container, method, action){
+    this.container = document.querySelector(container);
+    this.method = method;
+    this.action = action;
+  }
+  addItem(item){
+    this.items.push(item);
+  }
+  render(){
+    let formElement = document.createElement('form');
+    formElement.setAttribute('method', this.method);
+    formElement.setAttribute('action', this.action)
+
+    for(let i in this.items){
+      this.items[i].render(formElement)
+    }
+
+
+    this.container.appendChild(formElement);
+  }
+}
 class Input {
   requared = false;
   _type = 'text';
@@ -9,30 +33,54 @@ class Input {
     return this._type;
   }
   set type(t){
-    if(['email', 'text', 'password', 'submit'].includes(t)){
-      this._type;
+    if(['text', 'password', 'email', 'submit'].includes(t)){
+      this._type = t;
     }else{
       throw new Error(`Input ${t} type doesn't exist`)
     }
   }
+
+  render(formElement){
+    let el = document.createElement('input')
+    el.type = this.type;
+    el.name = this.name;
+    el.placeholder = this.label;
+    el.required = this.requared;
+    formElement.appendChild(el);
+  }
 }
-class Button{
+class Button extends Input{
   constructor(label){
     super('', label);
     this.type = 'submit';
   }
+  render(formElement){
+    let el = document.createElement('input');
+    el.type = this.type;
+    el.value = this.label;
+    formElement.appendChild(el);
+  }
 }
 // implementação
+
+// formulário
+let form = new Form('.formArea', 'POST', 'https://site.com.br')
 
 // email
 
 let email = new Input('email', 'Digite seu email');
 email.type = 'email'
 email.requared = true;
+form.addItem(email);
 
 // senha
 let senha = new Input('senha', 'Digite sua senha');
 senha.type = 'password';
+form.addItem(senha);
 
 // button
-let button = new Input('Enviar'); 
+let button = new Button('Enviar'); 
+form.addItem(button);
+
+console.log(form.items);
+form.render()
